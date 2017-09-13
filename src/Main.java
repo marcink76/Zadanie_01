70e3842bd99ef61a8c1bb0d929aa49d520e0667b
Marcin Konieczny <marcin_k@coni.pl>
2017-09-13 17:46:20 +0200

    Version 0.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("Programować");
+    }
+}
